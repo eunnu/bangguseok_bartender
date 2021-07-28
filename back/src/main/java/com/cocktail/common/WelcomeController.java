b04e13b416a0ae4b1152ac0933c5cbd82a9264a7
gk1vk <gk1vk@naver.com>
2021-07-28 23:07:27 +0900

    feat(back): init

---
+++ back/src/main/java/com/cocktail/common/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.cocktail.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class WelcomeController {
+    @GetMapping("/")
+    public String welcome(){
+        return "index";
+    }
+}
