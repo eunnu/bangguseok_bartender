297103f220ba47513183413f531f073c62e9b675
gk1vk <gk1vk@naver.com>
2021-08-16 20:38:46 +0900

    feat(back): cocktail dto 추가

---
+++ back/src/main/java/com/cocktail/dto/CocktailRequest.java
@@ -0,0 +1,33 @@
+package com.cocktail.dto;
+
+import com.cocktail.domain.Cocktail;
+import com.cocktail.domain.Glass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class CocktailRequest {
+
+    private String name;
+
+    private String description;
+
+    private List<Long> ingredientIdList = new ArrayList<>();
+
+    private List<Double> quantityList = new ArrayList<>();
+
+    private String glass;
+
+    private Long userId;
+
+    public Cocktail toCocktail() {
+        return Cocktail.builder()
+                .name(name).description(description)
+                .glass(Glass.valueOf(glass)).build();
+    }
+
+}
