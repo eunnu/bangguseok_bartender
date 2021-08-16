6b69bdb59ae85ddd2c0cc1ec5bfceab192e674b8
gk1vk <gk1vk@naver.com>
2021-08-16 23:24:41 +0900

    feat(back): create cocktail api 추가

--- back/src/main/java/com/cocktail/controller/CocktailController.java
+++ back/src/main/java/com/cocktail/controller/CocktailController.java
@@ -1,12 +1,12 @@
 package com.cocktail.controller;
 
+import com.cocktail.dto.CocktailRequest;
 import com.cocktail.dto.CocktailResponse;
 import com.cocktail.service.CocktailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/cocktails")
@@ -19,5 +19,10 @@ public class CocktailController {
     @ResponseBody
     public CocktailResponse findCocktail(Long id) {
         return cocktailService.findCocktail(id);
+    }
+
+    @PostMapping(value = "/new", params = "userId")
+    public Long createCocktail(@Valid Long userId, @RequestBody @Valid CocktailRequest cocktailRequest) {
+        return cocktailService.createCocktail(userId, cocktailRequest);
     }
 }
